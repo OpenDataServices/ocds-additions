673f4063f5cd6b8af40d5e11d2eb28375aa28dc3
James B <james.baster@opendataservices.coop>
2025-04-10 16:41:04 +0100

    Drop Python 3.7 and 3.8 support

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         ]
     },
     include_package_data=True,
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     install_requires=[
         "ocdskit>=1.0.3,<1.1",
         "ocdsextensionregistry>=0.1.10,<0.2",
